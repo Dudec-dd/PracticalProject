763fcbf5becb14ae4adf7ab45dfba18d09441c55
Андрей <@DESKTOP-C3Q8NL6>
2023-05-30 16:20:44 +0400

    Добавьте файлы проекта.

---
+++ PracticalProject/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticalProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AdminPage());
+        }
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(LoginTBox.Text == "1" && PasswordTBox.Text == "1")
+            {
+                NavigationService.Navigate(new AdminPage());
+            }
+        }
+    }
+}
